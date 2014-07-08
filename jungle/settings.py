600e5329e35bad55878af9c33ba2662a97bb3777
Sahil Patel <spatel7@berkeley.edu>
2014-07-08 00:30:22 -0700

    Removed lines from settings.py

--- jungle/settings.py
+++ jungle/settings.py
@@ -107,9 +107,6 @@ TEMPLATE_LOADERS = (
 LOGIN_REDIRECT_URL = '/home/'
 LOGIN_URL = '/login/'
 
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
